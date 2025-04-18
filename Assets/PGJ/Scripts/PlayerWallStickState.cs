43bcf762da47ded5084b01c2a8d7195f331365c7
pgj234 <pgj94@naver.com>
2025-04-18 13:23:29 +0900

    PGJ
    1. 씬마스터 스크립트 이름수정
    2. 플레이어 벽 붙기 상태 스크립트 추가

---
+++ Assets/PGJ/Scripts/PlayerWallStickState.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class PlayerWallStickState : PlayerState
+{
+    public PlayerWallStickState(Player _player, PlayerStateMachine _stateMachine, string _animBoolName) : base(_player, _stateMachine, _animBoolName)
+    {
+    }
+
+
+}
