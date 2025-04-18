a1244b15eb4fa5b1e9af8a78a5da37553aaec1cf
SDLulu <52689979+SDLulu@users.noreply.github.com>
2025-04-18 13:12:01 +0900

    KYW
    InputManager에 입력받는거 정지/시작 함수추가랑 마우스클릭 bool변수 추가, kyw씬 수정

--- Assets/KYW/Scripts/Mirror.cs
+++ Assets/KYW/Scripts/Mirror.cs
@@ -1,19 +1,33 @@
 ﻿using UnityEngine;
+using DG.Tweening;
 
 public class Mirror : MonoBehaviour
 {
+    private bool isRotating = false;
+    public float rotateDuration = 0.3f;
+
     private void OnTriggerEnter2D(Collider2D other)
     {
-        // 특정 태그 가진 물체에 반응
+        // 회전 중이면 무시
+        if (isRotating) return;
+
         if (other.CompareTag("Sword"))
         {
             Debug.Log("칼이 닿았어요!");
-            Rotate30();
+            Rotate45();
         }
     }
-    void Rotate30()
+
+    void Rotate45()
     {
-        // 원하는 행동
-        Debug.Log("패링");
+        isRotating = true;
+
+        transform
+            .DORotate(new Vector3(0f, 0f, transform.eulerAngles.z + 45f), rotateDuration)
+            .SetEase(Ease.OutBack)
+            .OnComplete(() =>
+            {
+                isRotating = false; // 다시 충돌 가능!
+            });
     }
 }
--- Assets/PGJ/Scripts/InputManager.cs
+++ Assets/PGJ/Scripts/InputManager.cs
@@ -1,4 +1,4 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 public class InputManager : MonoBehaviour
 {
@@ -7,6 +7,11 @@ public class InputManager : MonoBehaviour
     internal float xInput { get; private set; }
     internal float yInput { get; private set; }
     internal float jump { get; private set; }
+
+    internal bool leftClick { get; private set; }
+    internal bool rightClick { get; private set; }
+
+    private bool inputEnabled = true;
 
     void Awake()
     {
@@ -24,8 +29,37 @@ public class InputManager : MonoBehaviour
 
     void Update()
     {
+        if (!inputEnabled)
+        {
+            xInput = 0f;
+            yInput = 0f;
+            jump = 0f;
+            leftClick = false;
+            rightClick = false;
+            return;
+        }//인풋안받을때는 초기화
+
         xInput = Input.GetAxis("Horizontal");
         yInput = Input.GetAxis("Vertical");
         jump = Input.GetAxisRaw("Jump");
+
+        //좌우클릭 했는지
+        leftClick = Input.GetMouseButtonDown(0);
+        rightClick = Input.GetMouseButtonDown(1);
+    }
+
+    public void InputStop()
+    {
+        inputEnabled = false;
+    }
+
+    public void InputStart()
+    {
+        inputEnabled = true;
+    }
+
+    public bool IsInputEnabled()
+    {
+        return inputEnabled;
     }
 }
