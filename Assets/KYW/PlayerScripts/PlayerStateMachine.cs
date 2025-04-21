a1611e03decf3b071fdcec72e643ed9da6b2395d
SDLulu <52689979+SDLulu@users.noreply.github.com>
2025-04-21 17:04:51 +0900

    폴더의정상화
    폴더의정상화

---
+++ Assets/KYW/PlayerScripts/PlayerStateMachine.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class PlayerStateMachine
+{
+    public PlayerState currentState { get; private set; }
+
+    // 초기화
+    public void Init(PlayerState _startState)
+    {
+        currentState = _startState;
+        currentState.Enter();
+    }
+
+    public void ChangeState(PlayerState _newState)
+    {
+        currentState.Exit();
+        currentState = _newState;
+        currentState.Enter();
+    }
+}
