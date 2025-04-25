efdfc14e847f337024090d67c8254ebea08f1374
SDLulu <52689979+SDLulu@users.noreply.github.com>
2025-04-25 17:24:01 +0900

    asdf

--- Assets/KYW/PlayerStateScripts/PlayerMoveState.cs
+++ Assets/KYW/PlayerStateScripts/PlayerMoveState.cs
@@ -30,6 +30,12 @@ public class PlayerMoveState : PlayerState
         {
             player.stateMachine.ChangeState(player.jumpState);
         }
+
+        //하강 감지 → 낙하 상태로 전환
+        if (rb.linearVelocityY < 0f)
+        {
+            stateMachine.ChangeState(player.fallState);
+        }
     }
 
     public override void Exit()
