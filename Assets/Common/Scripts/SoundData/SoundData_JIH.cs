9232acc15f7d0039455a4fe3b76681c5f65f873c
pgj234 <pgj94@naver.com>
2025-05-01 17:31:58 +0900

    PGJ
    사운드 시스템 개선

---
+++ Assets/Common/Scripts/SoundData/SoundData_JIH.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+// BGM 목록 enum (넣은 배열 순서대로)
+public enum BGM_JIH
+{
+
+}
+
+// SFX 목록 enum (넣은 배열 순서대로)
+public enum SFX_JIH
+{
+
+}
+
+public class SoundData_JIH : ScriptableObject
+{
+    [SerializeField] internal AudioClip[] bgmClip;
+    [SerializeField] internal AudioClip[] sfxClip;
+}
---
+++ Assets/Common/Scripts/SoundData/SoundData_NSH.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+// BGM 목록 enum (넣은 배열 순서대로)
+public enum BGM_NSH
+{
+
+}
+
+// SFX 목록 enum (넣은 배열 순서대로)
+public enum SFX_NSH
+{
+
+}
+
+public class SoundData_NSH : ScriptableObject
+{
+    [SerializeField] internal AudioClip[] bgmClip;
+    [SerializeField] internal AudioClip[] sfxClip;
+}
