7011f270b1a2ab9d2b6641b188e124886c811cf7
양사무엘 <109801997+YangSamuel726@users.noreply.github.com>
2025-05-01 13:51:18 +0900

    YSME
    사운드 에셋 추가

--- Assets/YSME/Scripts/RepeatBackground.cs
+++ Assets/YSME/Scripts/RepeatBackground.cs
@@ -2,18 +2,30 @@ using UnityEngine;
 
 public class RepeatBackground : MonoBehaviour
 {
-    private SpriteRenderer spriteRenderer;
+    private MeshRenderer meshRenderer;
     private PlayerManager pm;
+
+    [Header("스크롤 스피드")]
+    [SerializeField] private float scrollSpeed;
 
     void Start()
     {
-        spriteRenderer = GetComponent<SpriteRenderer>();
+        meshRenderer = GetComponent<MeshRenderer>();
         pm = PlayerManager.Instance;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (pm.player.GetVelocity().x < 0)
+        {
+            Vector2 offset = new Vector2(meshRenderer.material.mainTextureOffset.x - Time.deltaTime * scrollSpeed, 0);
+            meshRenderer.material.mainTextureOffset = offset;
+        }
+        else if (pm.player.GetVelocity().x > 0)
+        {
+            Vector2 offset = new Vector2(meshRenderer.material.mainTextureOffset.x + Time.deltaTime * scrollSpeed, 0);
+            meshRenderer.material.mainTextureOffset = offset;
+        }
     }
 }
