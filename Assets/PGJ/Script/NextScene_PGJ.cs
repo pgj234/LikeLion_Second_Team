769ae1ea1b04160cbfd2dd66c19d55580a8acedf
pgj234 <pgj94@naver.com>
2025-05-01 14:38:28 +0900

    PGJ
    JIH 씬으로 연결

---
+++ Assets/PGJ/Script/NextScene_PGJ.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;  // UI 요소를 사용하려면 이 네임스페이스가 필요합니다.
+
+public class NextScene_PGJ : MonoBehaviour
+{
+    public string targetSceneName = "JIH";
+    public FadeController fadeController;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            fadeController.FadeOutAndLoad(targetSceneName);
+        }
+    }
+}
