912d90a3693b7975742ed5adf8fbdaee7cb5aec0
양사무엘 <109801997+YangSamuel726@users.noreply.github.com>
2025-05-01 17:13:52 +0900

    YSME

--- Assets/YSME/Scripts/FadeInOut.cs
+++ Assets/YSME/Scripts/FadeInOut.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using Microsoft.Unity.VisualStudio.Editor;
 using UnityEngine;
 
@@ -5,4 +6,29 @@ using UnityEngine;
 public class FadeInOut : MonoBehaviour
 {
     private Image image;
+
+    void Start()
+    {
+        // EventManager.instance.OnPlayerRespawned
+    }
+
+    public void FadeIn()
+    {
+        StartCoroutine(StartFadeIn());
+    }
+
+    public void FadeOut()
+    {
+        StartCoroutine(StartFadeOut());
+    }
+
+    IEnumerator StartFadeIn()
+    {
+        yield return null;
+    }
+
+    IEnumerator StartFadeOut()
+    {
+        yield return null;
+    }
 }
