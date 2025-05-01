eece909faef669e4484224885af410e8892c017a
pgj234 <pgj94@naver.com>
2025-05-01 20:02:04 +0900

    PGJ
    1. 난이도 조절
    2. 임시 BGM 재생

---
+++ Assets/PGJ/Script/BGM_Start_PGJ.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class BGM_Start_PGJ : MonoBehaviour
+{
+    void Start()
+    {
+        SoundManager.instance.StopBGM();
+        SoundManager.instance.PlayBGM(BGM_PGJ.Bgm);
+    }
+}
