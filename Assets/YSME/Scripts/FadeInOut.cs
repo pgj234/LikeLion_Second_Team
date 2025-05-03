6fea9062a486d7d886a8caf6dddbe3ae86c24912
양사무엘 <109801997+YangSamuel726@users.noreply.github.com>
2025-05-03 15:59:35 +0900

    YSME

--- Assets/YSME/Scripts/FadeInOut.cs
+++ Assets/YSME/Scripts/FadeInOut.cs
@@ -1,34 +1,63 @@
 using System.Collections;
-using Microsoft.Unity.VisualStudio.Editor;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 [RequireComponent(typeof(Image))]
 public class FadeInOut : MonoBehaviour
 {
+    [Header("페이드 설정")]
     private Image image;
+    [SerializeField] private Color fadeInColor;
+    [SerializeField] private float fadeInTime;
+    [SerializeField] private Color fadeOutColor;
+    [SerializeField] private float fadeOutTime;
+
+    void Awake()
+    {
+        image = GetComponent<Image>();
+    }
 
     void Start()
     {
-        // EventManager.instance.OnPlayerRespawned
+        FadeIn();
     }
 
     public void FadeIn()
     {
+        StopCoroutine(StartFadeOut());
         StartCoroutine(StartFadeIn());
     }
 
     public void FadeOut()
     {
+        StopCoroutine(StartFadeIn());
         StartCoroutine(StartFadeOut());
     }
 
     IEnumerator StartFadeIn()
     {
-        yield return null;
+        float curTime = 0;
+        float percent = 0;
+        while (curTime < fadeInTime)
+        {
+            curTime += Time.deltaTime;
+            percent = curTime / fadeInTime;
+            image.color = Vector4.Lerp(fadeOutColor, fadeInColor, percent);
+            yield return null;
+        }
     }
 
     IEnumerator StartFadeOut()
     {
-        yield return null;
+        float curTime = 0;
+        float percent = 0;
+        while (curTime < fadeOutTime)
+        {
+            curTime += Time.deltaTime;
+            percent = curTime / fadeOutTime;
+            image.color = Vector4.Lerp(fadeInColor, fadeOutColor, percent);
+            yield return null;
+        }
     }
 }
