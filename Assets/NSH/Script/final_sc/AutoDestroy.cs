1192628825f7fd6a091348141d5e8c6246b28e5c
on-34 <onkn34@naver.com>
2025-05-09 17:22:04 +0900

    NSH
    최종 4차

--- Assets/NSH/Script/final_sc/AutoDestroy.cs
+++ Assets/NSH/Script/final_sc/AutoDestroy.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class AutoDestroy : MonoBehaviour
 {
-    public float lifetime = 7f; // 몇 초 뒤에 삭제할지 설정
+    public float lifetime = 4f; // 몇 초 뒤에 삭제할지 설정
 
     void Start()
     {
