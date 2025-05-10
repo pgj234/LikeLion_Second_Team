7661137dbfb8352e6db8aad28d749eebe786e7b9
SDLulu <52689979+SDLulu@users.noreply.github.com>
2025-05-10 18:38:21 +0900

    asdf
    asdf

--- Assets/KYW/SceneScripts/LaserReciever.cs
+++ Assets/KYW/SceneScripts/LaserReciever.cs
@@ -3,12 +3,15 @@
 public class LaserReceiver : MonoBehaviour
 {
     public GameObject obj; // 반응할 오브젝트
+    private bool isActive = true; //수신기
 
     public void Activate()
     {
+        if(!isActive) return;
         // Debug.Log("수신기 작동됨!");
         // // 반응할거
         obj.SetActive(true);
         SoundManager.instance.PlaySFX(SFX_KYW.CandleLight);
+        isActive = false;
     }
 }
