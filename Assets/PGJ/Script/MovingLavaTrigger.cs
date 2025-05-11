b351902bc8118d2533752704cf4304cd939a2e8f
pgj234 <pgj94@naver.com>
2025-05-11 17:28:31 +0900

    PGJ
    무빙 라바 트리거 스크립트 추가

---
+++ Assets/PGJ/Script/MovingLavaTrigger.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class MovingLavaTrigger : MonoBehaviour
+{
+    [SerializeField] MovingLava movingLava;
+    [SerializeField] float targetLavaSpd;
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            MovingLavaSpeedSet();
+
+            enabled = false;
+        }
+    }
+
+    void MovingLavaSpeedSet()
+    {
+        if (false == movingLava.gameObject.activeSelf)
+        {
+            movingLava.gameObject.SetActive(true);
+        }
+
+        movingLava.parentMoveSpeed = targetLavaSpd;
+    }
+}
